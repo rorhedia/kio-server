69ef8161d3d0216c9780954bcef7293b1c7c3af0
Daniel Heredia <rorhedia@gmail.com>
2020-11-14 11:36:57 -0600

    Se agrega middleware para la validación del fomulario de ideas

---
+++ src/middlewares/ideas.js
@@ -0,0 +1,35 @@
+const yup = require("yup");
+
+function formValidation(req, res, next) {
+  try {
+    yup
+      .object()
+      .shape({
+        name: yup
+          .string("Solo se aceptan letras en el nombre de la idea")
+          .min(3, "El nombre debe contener almenos 3 letras")
+          .max(60)
+          .required("Debe agregar el nombre de la idea"),
+        description: yup
+          .string("No se aceptan caracteres extraños"),
+        effect: yup
+          .string("No se aceptan caracteres extraños"),
+        image: yup.string("No se aceptan caracteres extraños"),
+        file: yup.string("No se aceptan caracteres extraños"),
+      })
+      .validateSync(req.body);
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      data: {
+        name: error.name,
+        message: error.message,
+        path: error.path,
+      },
+    });
+  }
+}
+
+module.exports = { formValidation };
